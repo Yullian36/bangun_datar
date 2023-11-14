07b96b519e70f7b127aa48129c32ca54e16eecf5
Yullian36 <aduyulian@gmail.com>
2023-11-14 14:42:35 +0700

    custom componen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+import 'dart:ui_web';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,25 +19,38 @@ class HomePage extends StatelessWidget {
         ),
        body: Column(
          children: [
-           Container(
-             margin: EdgeInsets.all(8),
-             padding: EdgeInsets.symmetric(vertical: 8,horizontal: 16),
-             decoration: BoxDecoration(
-               color: Colors.blue
-             ),
-             child: Text("Persegi"),
-           ),
-           Container(
-             margin: EdgeInsets.all(8),
-             padding: EdgeInsets.symmetric(vertical: 8,horizontal: 16),
-             decoration: BoxDecoration(
-                 color: Colors.blue
-             ),
-             child: Text("Segitiga"),
-           ),
+           CustomMenu(imageAssets: "assets/img.png",title: "persegi"),
+           CustomMenu(imageAssets: "assets/img_1.png",title: "Trisula"),
+
+
          ],
        ),
 
+    );
+  }
+}
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({
+    super.key, required this.imageAssets, required this.title,
+  });
+final String imageAssets;
+final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.symmetric(vertical: 8,horizontal: 16),
+      decoration: BoxDecoration(
+        color: Colors.greenAccent
+      ),
+      child: Column(
+        children: [
+          Image.asset(imageAssets, height: 150,),
+         Text(title),
+        ],
+      ),
     );
   }
 }
